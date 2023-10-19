eb0ee239aea9e03741b4e59b6ec8c41d301c2f76
donnavh <118171372+donnavh@users.noreply.github.com>
2023-10-19 13:10:52 +0200

    Add files via upload

--- basiswebsite fed/scripts/script.js
+++ basiswebsite fed/scripts/script.js
@@ -1,12 +1,28 @@
 // JavaScript Document
 console.log("hi");
 
-var openButton = document.querySelector("header > button");
 
-openButton.onclick = openMenu;
+
+var openButton = document.querySelector("header > button:nth-of-type(2)");
+
 
 function openMenu() {
     var deNav = document.querySelector("nav");
 
     deNav.classList.add("toonMenu");
+    console.log("hrt werkt");
 }
+
+openButton.onclick = openMenu;
+
+
+var sluitButton = document.querySelector("nav button");
+
+sluitButton.onclick = sluitMenu;
+
+function sluitMenu(){
+    var deNav = document.querySelector("nav");
+    deNav.classList.remove("toonMenu");
+}
+
+
